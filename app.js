627e5884676aa8b2935545a8f7f4ee1b049b5984
Rajkishor Pradhan <rajkp7598@gmail.com>
2021-03-30 01:10:52 +0530

    Checking security features from mongoose

---
+++ app.js
@@ -0,0 +1,60 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// mongoose.connect("mongodb://localhost:27017/userDB", {
+mongoose.connect("mongodb+srv://rk-mongo:Rajkp@cluster0.vbjj1.mongodb.net/todoList", {
+    urlencoded: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/login", function (req, res) {
+    User.findOne({ email: req.body.username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser.password === req.body.password) {
+                res.render("secrets");
+            }
+        }
+    });
+});
+
+app.post("/register", function (req, res) {
+    let user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save();
+    res.render("secrets");
+});
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000");
+});
